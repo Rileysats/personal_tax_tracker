57955ff768e9bc9d30e2919c4f52e6313689090e
Rileysats <rileysatanek@icloud.com>
2024-08-07 18:06:41 +1000

    update logger to create logs file in root

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,15 @@
 import logging
+import os
+
+log_dir = "logs"
+log_file_name ="log.txt"
+
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, log_file_name)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-# ch = logging.StreamHandler()
-handler = logging.FileHandler("logs/log.txt")        
+handler = logging.FileHandler(log_file)    
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
